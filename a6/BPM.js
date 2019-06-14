823cd9578cf3a77dd9d812bcee680b4cf6df9ed2
creiland <creiland19@gmail.com>
2019-06-14 08:04:43 -0700

    preparing gh-pages"

--- a6/BPM.js
+++ a6/BPM.js
@@ -64,15 +64,71 @@ let currentKickIndex = 0;
 
 let currentAngle = 0;
 
+function updateArc(updatedAngle){
+
+    var tau = Math.PI * 2
+
+    var color = d3.scaleSequential(d3.interpolateInferno)
+
+    var arc = d3.arc()
+        .innerRadius(180)
+        .outerRadius(240)
+        .startAngle(0);
+
+    var svg = d3.selectAll('svg.arc')
+
+    var g = svg.selectAll('g.arc-cont')
+
+    var toChange = g.selectAll('path.new-arc')
+
+
+    if(updatedAngle > .25){
+        toChange.transition()
+            .duration(100)
+            .ease(d3.easeCircleIn)
+            .attrTween("d", arcTween(updatedAngle * tau));
+    } else {
+        g.selectAll('path.old-arc').remove();
+        toChange.attr('class', 'old-arc');
+        g.append("path")
+        .datum({ endAngle: 0 * tau })
+        .attr('class', 'new-arc')
+        .style("fill", color(Math.random()))
+        .transition()
+            .duration(100)
+            .ease(d3.easeCircleIn)
+            .attrTween("d", arcTween(updatedAngle * tau));
+    }
+    
+
+            function arcTween(newAngle) {
+                return function (d) {
+                    var interpolate = d3.interpolate(d.endAngle, newAngle);
+                    return function (t) {
+                        d.endAngle = interpolate(t);
+                        return arc(d);
+                    };
+                };
+            }
+}
+
 getBeat = () => {
     //console.log(kickPattern[currentKickIndex] % audioRef.currentTime)
-    if(kickPattern[currentKickIndex] - audioRef.currentTime < .01){
-        console.log(audioRef.currentTime)
-        console.log(kickPattern[currentKickIndex])
+    if(kickPattern[currentKickIndex] - audioRef.currentTime < .001){
         currentAngle += 0.25;
         if(currentAngle>1){
-            currentAngle = 0;
+            currentAngle = .25;
         }
+        d3.selectAll('svg.arc').selectAll('g').selectAll('text')
+        .transition()
+        .duration(5)
+        .style('opacity', .7)
+        .transition()
+        .duration(100)
+        .ease(d3.easeCircleIn)
+        .style('opacity', 1)
+
+        updateArc(currentAngle);
         currentKickIndex++;
     }
     var time = audioRef.currentTime / audioRef.duration
@@ -89,14 +145,6 @@ getBeat = () => {
     .attr('width', 2)
     .attr('fill', 'red')
 
-    var secondsBetweenBeats = Math.round(1000*(1/(+BPMText/60)))/1000
-    var currentTime = Math.round(1000*audioRef.currentTime)/1000
-
-    //console.log(currentTime % secondsBetweenBeats)
-    
-    if(currentTime % secondsBetweenBeats < 0.01){
-        
-    }
     requestAnimationFrame(getBeat)
 }
 
@@ -233,7 +281,6 @@ function offlineContext(buffer) {
         
         if(isPlaying){
             requestAnimationFrame(getBeat)
-            renderArc();
             document.getElementById('bpm-text').textContent = BPMText;
         }
     }
@@ -259,10 +306,11 @@ renderPeaks = (data, bufferLength) => {
         .attr("width", width)
         .attr("height", height)
 
-    // bar.append('rect')
-    //     .attr("width", width)
-    //     .attr("height", height)
-    //     .attr('color', "rgba(52,58,64, 1)")
+    bar.append('rect')
+        .attr("width", width)
+        .attr("height", height)
+        .attr('color', "rgba(52,58,64, 1)")
+        .style('z-index', '0')
 
     var g = bar.selectAll('rect')
         .data(data)
@@ -280,28 +328,27 @@ renderPeaks = (data, bufferLength) => {
         .style('z-index', '1000')
 }
 
-
 renderArc = () => {
     height = 500;
     width = 500;
 
     var tau = Math.PI * 2
 
+    var color = d3.scaleSequential(d3.interpolateInferno)
+
     var node = d3.select('#viz-container')
         .append('svg')
+        .attr('class', "arc")
         .attr("width", width)
         .attr("height", height)
 
-    var g = node.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+    var g = node.append("g")
+    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+    .attr('class', 'arc-cont')
 
     var arc = d3.arc()
         .innerRadius(180)
         .outerRadius(240)
-        .startAngle(0);
-
-    var loadingArc = d3.arc()
-        .innerRadius(90)
-        .outerRadius(120)
         .startAngle(0);
 
     var background = g.append("path")
@@ -312,7 +359,9 @@ renderArc = () => {
     // Add the foreground arc in orange, currently showing 12.7%.
     var foreground = g.append("path")
         .datum({ endAngle: 0 * tau })
-        .style("fill", "orange")
+        .attr('class', 'new-arc')
+        .attr('id', 'new-arc')
+        .style("fill", color(Math.random()))
         .attr("d", arc);
 
     g.append('text')
@@ -322,8 +371,6 @@ renderArc = () => {
         .style("text-anchor", "middle")
         .style('alignment-baseline', 'middle');
 
-    var angle = 0;
-
     // d3.interval(function () {
     //     foreground.transition()
     //         .duration(BPMText / 60 * 1000 / 4)
@@ -332,68 +379,10 @@ renderArc = () => {
 
     // Returns a tween for a transition’s "d" attribute, transitioning any selected
     // arcs from their current angle to the specified new angle.
-    function arcTween(newAngle, loading) {
-
-        angle += 0.25
-        if (angle > 1) {
-            angle = 0;
-        }
-
-        if (!newAngle) {
-            var newAngle = angle * tau;
-        }
-
-        // The function passed to attrTween is invoked for each selected element when
-        // the transition starts, and for each element returns the interpolator to use
-        // over the course of transition. This function is thus responsible for
-        // determining the starting angle of the transition (which is pulled from the
-        // element’s bound datum, d.endAngle), and the ending angle (simply the
-        // newAngle argument to the enclosing function).
-        return function (d) {
-
-            // To interpolate between the two angles, we use the default d3.interpolate.
-            // (Internally, this maps to d3.interpolateNumber, since both of the
-            // arguments to d3.interpolate are numbers.) The returned function takes a
-            // single argument t and returns a number between the starting angle and the
-            // ending angle. When t = 0, it returns d.endAngle; when t = 1, it returns
-            // newAngle; and for 0 < t < 1 it returns an angle in-between.
-            var interpolate = d3.interpolate(d.endAngle, newAngle);
-
-            // The return value of the attrTween is also a function: the function that
-            // we want to run for each tick of the transition. Because we used
-            // attrTween("d"), the return value of this last function will be set to the
-            // "d" attribute at every tick. (It’s also possible to use transition.tween
-            // to run arbitrary code for every tick, say if you want to set multiple
-            // attributes from a single function.) The argument t ranges from 0, at the
-            // start of the transition, to 1, at the end.
-            return function (t) {
-
-                // Calculate the current arc angle based on the transition time, t. Since
-                // the t for the transition and the t for the interpolate both range from
-                // 0 to 1, we can pass t directly to the interpolator.
-                //
-                // Note that the interpolated angle is written into the element’s bound
-                // data object! This is important: it means that if the transition were
-                // interrupted, the data bound to the element would still be consistent
-                // with its appearance. Whenever we start a new arc transition, the
-                // correct starting angle can be inferred from the data.
-                d.endAngle = interpolate(t);
-
-                // Lastly, compute the arc path given the updated data! In effect, this
-                // transition uses data-space interpolation: the data is interpolated
-                // (that is, the end angle) rather than the path string itself.
-                // Interpolating the angles in polar coordinates, rather than the raw path
-                // string, produces valid intermediate arcs during the transition.
-                if (loading) {
-                    return loadingArc(d)
-                }
-                return arc(d);
-            };
-        };
-    }
 }
 
     audioRef = document.getElementById("audio")
+    renderArc();
 
     // audioRef.onloadedmetadata = function() {
     //     fetch(audioFile)
